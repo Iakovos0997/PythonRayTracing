862da0f72d0aac2ee8aa06513979385588fb6aba
Jimmy <jimmycarrero@protonmail.com>
2025-08-24 13:35:10 -0400

    Added base objects raytrace and render functions

--- main.py
+++ main.py
@@ -1,50 +1,90 @@
+from graphics import *
 import math
 
-Cw, Ch = 10, 10
-d = 1
-Vw, Vh = 1, 1
-O = (0, 0, 0)
-inf = math.inf
+# ---------- Ray Tracer Core ----------
 
+def dot(a, b): return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+def sub(a, b): return (a[0]-b[0], a[1]-b[1], a[2]-b[2])
 
-def CanvasToViewport(x, y):
+def canvas_to_viewport(x, y, Vw, Vh, d, Cw, Ch):
     return (x * Vw / Cw, y * Vh / Ch, d)
 
-def TraceRay(O, D, t_min, t_max):
-    closest_t = inf
-    closest_sphere = None
-    for sphere in scene.spheres:
-        t1, t2 = IntersectRaySphere(O, D, sphere)
-        if t1 in [t_min, t_max] and t1 < closest_t:
-            closest_t = t1
-            closest_sphere = sphere
-        if t2 in [t_min, t_max] and t2 < closest_t:
-            closest_t = t2
-            closest_sphere = sphere
-    if closest_sphere == None:
-        return BACKGROUND_COLOR
-    return closest_sphere.color
+def rgb_to_hex(color):
+    return "#%02x%02x%02x" % color
 
-def IntersectRaySphere(O, D, sphere):
-    r = sphere.radio
-    CO = O - sphere.center
-   
-    a = dot(D, D)
-    b = 2*dot(CO, D)
-    c = dot(CO, CO) - r*r
-   
-    discriminant = b*b - 4*a*c
-    if discriminant < 0:
-        return inf, inf
-   
-    t1 = (-b + sqrt(discriminante)) // (2*a)
-    t2 = (-b - sqrt(discriminante)) // (2*a)
+def trace_ray(O, D, t_min, t_max, objects, background=(255,255,255)):
+    closest_t = float('inf')
+    closest_obj = None
 
-    return t1, t2
+    for obj in objects:
+        ts = obj.intersect(O, D)   # object defines its own intersection
+        if ts is None:
+            continue
+        for t in ts:
+            if t_min <= t <= t_max and t < closest_t:
+                closest_t = t
+                closest_obj = obj
 
+    if closest_obj is None:
+        return background
+    return closest_obj.color
 
-for x in range (-Cw//2, Cw//2):
-    for y in range(-Ch//2, Ch//2):
-        D = CanvasToViewport(x, y)
-        color = TraceRay(O, D, 1, inf)
-        canvas.PutPixel(x, y, color) 
+# ---------- Object Abstractions ----------
+
+class SceneObject:
+    """Base class: requires intersect(O, D) and color"""
+    def __init__(self, color):
+        self.color = color
+    def intersect(self, O, D):
+        raise NotImplementedError("Subclass must implement intersect")
+
+class Sphere(SceneObject):
+    def __init__(self, center, radius, color):
+        super().__init__(color)
+        self.center = center
+        self.radius = radius
+
+    def intersect(self, O, D):
+        CO = sub(O, self.center)
+        a = dot(D, D)
+        b = 2 * dot(CO, D)
+        c = dot(CO, CO) - self.radius**2
+        discriminant = b*b - 4*a*c
+        if discriminant < 0:
+            return None
+        sqrt_disc = math.sqrt(discriminant)
+        t1 = (-b + sqrt_disc) / (2*a)
+        t2 = (-b - sqrt_disc) / (2*a)
+        return (t1, t2)
+
+# ---------- Renderer ----------
+
+def render(win, width, height, objects):
+    O = (0,0,0)
+    Vw, Vh, d = 1.0, 1.0, 1.0
+    for y_img in range(height):
+        y = (height/2 - y_img)
+        for x_img in range(width):
+            x = x_img - width/2
+            D = canvas_to_viewport(x, y, Vw, Vh, d, width, height)
+            color = trace_ray(O, D, 1.0, float('inf'), objects)
+            win.plot(x_img, y_img, rgb_to_hex(color))
+
+# ---------- Example Usage ----------
+
+def main():
+    width, height = 400, 400
+    win = GraphWin("Object-Agnostic Raytracer", width, height, autoflush=False)
+
+    objects = [
+        Sphere((0, -1, 3), 1, (255, 0, 0)),   # Red
+        Sphere((2,  0, 4), 1, (0, 0, 255)),   # Blue
+        Sphere((-2, 0, 4), 1, (0, 255, 0)),   # Green
+    ]
+
+    render(win, width, height, objects)
+    win.getMouse()
+    win.close()
+
+if __name__ == "__main__":
+    main()
