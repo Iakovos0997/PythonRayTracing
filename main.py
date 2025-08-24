f7d48ced817fc9dfb7b3b217bf04d00c76521b5a
Jimmy <jimmycarrero@protonmail.com>
2025-08-24 16:58:36 -0400

    Added a cylinder to be rendered.

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ def main():
         Sphere(Vector(0, -1, 3), 1, (255, 0, 0), 500),   # Red
         Sphere(Vector(2,  0, 4), 1, (0, 0, 255), 500),   # Blue
         Sphere(Vector(-2, 0, 4), 1, (0, 255, 0), 10),   # Green,
-        Sphere(Vector(0, -5001, 0), 5000, (255, 255, 0), 1000) # Yellow
+        Sphere(Vector(0, -5001, 0), 5000, (255, 255, 0), 1000), # Yellow
+        # Horizontal magenta cylinder just above the origin
+        Cylinder(
+            base_center=Vector(-1, 1, 5),  # slightly above origin
+            axis=Vector(1, 0, 0),           # horizontal along X-axis
+            radius=0.5,
+            height=2,                      # length along X-axis
+            color=(255, 0, 255),             # magenta
+            specular=500
+        )
     ]
 
     lights = [
